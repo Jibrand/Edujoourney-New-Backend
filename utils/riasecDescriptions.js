c11e8f1a833844772cb7e0ee2eb7d9f60be375cb
Jibrand <muhammadjibrandev@gmail.com>
2026-01-06 16:13:15 +0500

    j

--- utils/riasecDescriptions.js
+++ utils/riasecDescriptions.js
@@ -1,26 +1,26 @@
 export  const riasecDescriptions = {
     r: {
         name: 'Realistic',
-        description: 'A person with an "Realistic Personality" tends to be frank, practical, focused, mechanical, determined, and rugged. Examples of realistic abilities include manipulating tools, doing mechanical or manual tasks, and doing athletic activities.'
+        description: 'tends to be frank, practical, focused, mechanical, determined, and rugged. Examples of realistic abilities include manipulating tools, doing mechanical or manual tasks, and doing athletic activities.'
     },
     i: {
         name: 'Investigative',
-        description: 'A person with an "Investigative Personality" tends to be analytical, intellectual, reserved, independent, and scholarly. Examples of investigative abilities include working with abstract ideas and intellectual problems.'
+        description: 'tends to be analytical, intellectual, reserved, independent, and scholarly. Examples of investigative abilities include working with abstract ideas and intellectual problems.'
     },
     a: {
         name: 'Artistic',
-        description: 'A person with an "Artistic Personality" tends to be complicated, original, impulsive, independent, expressive, and creative. Examples of artistic abilities include using imagination and feelings in creative expression.'
+        description: 'tends to be complicated, original, impulsive, independent, expressive, and creative. Examples of artistic abilities include using imagination and feelings in creative expression.'
     },
     s: {
         name: 'Social',
-        description: 'A person with an "Social Personality" tends to be helping, informing, teaching, inspiring, counseling, and serving. Examples of social abilities include interacting with people and concerned with the welfare of people.'
+        description: 'tends to be helping, informing, teaching, inspiring, counseling, and serving. Examples of social abilities include interacting with people and concerned with the welfare of people.'
     },
     e: {
         name: 'Enterprising',
-        description: 'A person with an "Enterprising Personality" tends to be persuasive, energetic, sociable, adventurous, ambitious, and risk-taking. Examples of enterprising abilities include leading, managing, and organizing.'
+        description: 'tends to be persuasive, energetic, sociable, adventurous, ambitious, and risk-taking. Examples of enterprising abilities include leading, managing, and organizing.'
     },
     c: {
         name: 'Conventional',
-        description: 'A person with an "Conventional Personality" tends to be careful, conforming, conservative, conscientious, self-controlled, and structured. Examples of conventional abilities include ordering activities and paying attention to details.'
+        description: 'tends to be careful, conforming, conservative, conscientious, self-controlled, and structured. Examples of conventional abilities include ordering activities and paying attention to details.'
     }
 };
